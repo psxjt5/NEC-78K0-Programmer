83fc2c9a14db1bb480ad085275cbbefb379bba26
James <psxjt5@nottingham.ac.uk>
2024-04-06 17:13:29 +0100

    Added header and pin definitions.

--- NEC-78K-Programmer.ino
+++ NEC-78K-Programmer.ino
@@ -1,5 +1,17 @@
-void setup() {
+/*
+    Arduino-Based NEC 78K Programmer
+    06/04/2024
+    James.Todd@nottingham.ac.uk
+*/
 
+// Pin Definitons
+#define PROG_SCK    = 2
+#define PROG_RX     = 3
+#define PROG_TX     = 4
+#define PROG_RESET  = 5
+
+void setup() {
+    
 }
 
 void loop() {
