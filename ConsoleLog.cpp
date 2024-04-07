e51f5e4f84f520699d7f63b2e17ca4d24bcc4e50
James <psxjt5@nottingham.ac.uk>
2024-04-07 11:27:52 +0100

    Debug Output Added.

--- ConsoleLog.cpp
+++ ConsoleLog.cpp
@@ -1,14 +1,22 @@
 #include "ConsoleLog.h"
 
 bool Enabled = false;
+bool Debug = false;
 
-void InitialiseConsole(int BaudRate) {
+void InitialiseConsole(int BaudRate, bool DebugOutput) {
     Serial.begin(BaudRate);
     Enabled = true;
+    Debug = DebugOutput;
 }
 
 void OutputToConsole(String Text) {
     if (Enabled) {
         Serial.println(Text);
+    }
+}
+
+void OutputToConsoleDebug(String Text) {
+    if (Enabled && Debug) {
+        Serial.println("DEBUG: " + Text);
     }
 }
--- ConsoleLog.h
+++ ConsoleLog.h
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 
 extern bool Enabled = false;
+extern bool DebugOutput = false;
 
-void InitialiseConsole(int BaudRate = 9600);
+void InitialiseConsole(int BaudRate = 9600, bool DebugOutput = false);
 void OutputToConsole(String Text);
