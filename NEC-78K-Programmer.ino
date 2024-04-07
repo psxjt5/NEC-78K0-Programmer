f243f4cf08c7b19c513163a2799bbb3ea962ec01
James <psxjt5@nottingham.ac.uk>
2024-04-07 10:17:02 +0100

    Relay Triggering Investigated.

--- NEC-78K-Programmer.ino
+++ NEC-78K-Programmer.ino
@@ -5,15 +5,23 @@
 */
 
 // Pin Definitons
-#define PROG_SCK    = 2
-#define PROG_RX     = 3
-#define PROG_TX     = 4
-#define PROG_RESET  = 5
+#define     PROG_SCK        2       // Connects to: TX
+#define     PROG_RX         3       // Connects to: SO10
+#define     PROG_TX         4       // Connects to: RX
+#define     PROG_RESET      5       // Connects to: RESET
+#define     PROG_EN         6       // Connects to: VPP
+#define     PROG_VDD        7       // Connects to: VDD
 
 void setup() {
-    
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(PROG_EN, OUTPUT);
 }
 
 void loop() {
-
+    digitalWrite(LED_BUILTIN, HIGH);
+    //digitalWrite(PROG_EN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    //digitalWrite(PROG_EN, LOW);
+    delay(1000);
 }
